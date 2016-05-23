27058a6711abfc0337901d6f1d08a0915e816a3f
Jaap Jansma <jaap@edeveloper.nl>
2016-05-23 12:11:17 +0200

    #3383: sector token aangepast zodat deze gebruik maakt van de contact segment

--- CRM/Tokens/SectorCoordinator.php
+++ CRM/Tokens/SectorCoordinator.php
@@ -2,6 +2,15 @@
 
 class CRM_Tokens_SectorCoordinator extends CRM_Tokens_CaseRelationship {
 
+  private $sc_role;
+
+  public function __construct($relationship_type_name_a_b, $token_name, $token_label, $case_id = NULL) {
+    parent::__construct($relationship_type_name_a_b, $token_name, $token_label);
+
+    $segment_roles = civicrm_api3('OptionGroup', 'getvalue', array('name' => 'civicoop_segment_role', 'return' => 'id'));
+    $this->sc_role = civicrm_api3('OptionValue', 'getvalue', array('name' => 'sector_coordinator', 'option_group_id' => $segment_roles, 'return' => 'value'));
+  }
+
   public static function tokens(&$tokens, $token_name, $token_label) {
     CRM_Tokens_CaseRelationship::tokens($tokens, $token_name, $token_label);
     $tokens[$token_name][$token_name.'.sector'] = ts('Sector of '.$token_label);
@@ -9,26 +18,21 @@ class CRM_Tokens_SectorCoordinator extends CRM_Tokens_CaseRelationship {
 
   public function tokenValues(&$values, $cids, $job = null, $tokens = array(), $context = null) {
     parent::tokenValues($values, $cids, $job, $tokens, $context);
-    $this->sectorToken($values, $cids, 'sector');
+    if ($this->isTokenInTokens($tokens, 'sector')) {
+      $this->sectorToken($values, $cids, 'sector');
+    }
   }
 
   private function sectorToken(&$values, $cids, $token) {
     $sector = '';
     if ($this->contact_id) {
-      $dao = CRM_Core_DAO::executeQuery("SELECT t.*
-                                        FROM `civicrm_tag_enhanced` `e`
-                                        INNER JOIN `civicrm_tag` `t` ON `e`.`tag_id` = `t`.`id`
-                                        WHERE `e`.`coordinator_id` = %1
-                                        AND
-                                        (`start_date` IS NULL OR DATE(`start_date`) <= DATE(NOW()))
-                                        AND
-                                        (`end_date` IS NULL OR DATE(`end_date`) >= DATE(NOW()))
-                                        ", array(1 => array($this->contact_id, 'Integer')));
-      while($dao->fetch()) {
+      $contact_segments = civicrm_api3('ContactSegment', 'get', array('role_value' => $this->sc_role, 'is_active' => true));
+      foreach($contact_segments['values'] as $contact_segment) {
+        $segment = civicrm_api3('Segment', 'getsingle', array('id' => $contact_segment['segment_id']));
         if (strlen($sector)) {
           $sector .= ', ';
         }
-        $sector .= $dao->name;
+        $sector .= $segment['label'];
       }
     }
 
