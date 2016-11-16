78025dcddad303ea225064ad10d1fb4c4a05ed2e
Jaap Jansma <jaap@edeveloper.nl>
2016-11-16 14:23:38 +0100

    Fixed PUM Issue #3631 case tokens did not work with e-mail from civirules

--- CRM/Tokens/CaseId.php
+++ CRM/Tokens/CaseId.php
@@ -17,73 +17,90 @@
  */
 class CRM_Tokens_CaseId {
 
-    private static $caseId = false;
-    private static $parentCaseId = false;
+  private static $caseId = FALSE;
+  private static $parentCaseId = FALSE;
 
 
-    /**
-     * Check if the form has a case id and if so store the case id localy
-     * so we can use the case when we parse tokens to values
-     *
-     * @param $formName
-     * @param $form
-     */
-    public static function buildForm($formName, &$form) {
-        if (!empty($form->_caseId)) {
-            self::$caseId = $form->_caseId;
-            // now retrieve the parentCaseId as well
-            self::$parentCaseId = self::fetchParentCaseId(self::$caseId);
-        }
+  /**
+   * Check if the form has a case id and if so store the case id localy
+   * so we can use the case when we parse tokens to values
+   *
+   * @param $formName
+   * @param $form
+   */
+  public static function buildForm($formName, &$form) {
+    if (!empty($form->_caseId)) {
+      self::$caseId = $form->_caseId;
+      // now retrieve the parentCaseId as well
+      self::$parentCaseId = self::fetchParentCaseId(self::$caseId);
+    }
+  }
+
+  public static function getCaseId() {
+    return self::$caseId;
+  }
+
+  public static function getParentCaseId() {
+    return self::$parentCaseId;
+  }
+
+  public static function getCaseIdFromTokenValues($values, $cids) {
+    if (!empty(self::$caseId)) {
+      return;
     }
 
-    public static function getCaseId() {
-        return self::$caseId;
-    }
-	
-	  public static function getParentCaseId() {
-        return self::$parentCaseId;
-    }
-
-    public static function getCaseIdFromTokenValues($values, $cids) {
-        if (!empty(self::$caseId)) {
-            return;
-        }
-
-        $activity_id = false;
-        if (!is_array($cids) && isset($values['activity.activity_id'])) {
-            $activity_id = $values['activity.activity_id'];
-        } elseif (is_array($cids)) {
-            $cid = reset($cids);
-            if (isset($values[$cid]['activity.activity_id'])) {
-                $activity_id = $values[$cid]['activity.activity_id'];
-            }
-        }
-
-        if ($activity_id) {
-            self::$caseId = CRM_Core_DAO::singleValueQuery("SELECT case_id FROM civicrm_case_activity WHERE activity_id = %1", array(1=>array($activity_id, 'Integer')));
-            self::$parentCaseId = self::fetchParentCaseId(self::$caseId);
-        }
+    $case_id = FALSE;
+    $activity_id = FALSE;
+    if (!is_array($cids) && isset($values['case.id'])) {
+      $case_id = $values['case.id'];
+    } elseif (is_array($cids)) {
+      $cid = reset($cids);
+      if (isset($values[$cid]['case.id'])) {
+        $case_id = $values[$cid]['case.id'];
+      }
+    } elseif (!is_array($cids) && isset($values['activity.activity_id'])) {
+      $activity_id = $values['activity.activity_id'];
+    } elseif (is_array($cids)) {
+      $cid = reset($cids);
+      if (isset($values[$cid]['activity.activity_id'])) {
+        $activity_id = $values[$cid]['activity.activity_id'];
+      }
     }
 
-	public static function fetchParentCaseId($childCaseId) {
-        if(!$childCaseId) {
-            return false;
-        }
-		// retrieve custom table and - columns
-        $customGroup = civicrm_api3('CustomGroup', 'getsingle', array('name' => 'travel_parent'));
-        $customFields = civicrm_api3('CustomField', 'get', array('custom_group_id' => $customGroup['id']));
-        $cols = array();
-        foreach($customFields['values'] as $fld) {
-            $cols[] = $fld['column_name'] . ' AS ' . $fld['name'];
-        }
-        // build and execute query to retrieve parentCaseId from custom table
-        $sql = 'SELECT ' . implode(', ', $cols) . ' FROM ' . $customGroup['table_name'] . ' WHERE entity_id=' . $childCaseId;
-        $dao = CRM_Core_DAO::executeQuery($sql);
-        if ($dao->fetch()) {
-            // data retrieved: return parentCaseId
-            return $dao->case_id;
-        } else {
-            return false;
-        }
+    if ($case_id) {
+      self::$caseId = $case_id;
+      self::$parentCaseId = self::fetchParentCaseId(self::$caseId);
+    } elseif ($activity_id) {
+      self::$caseId = CRM_Core_DAO::singleValueQuery("SELECT case_id FROM civicrm_case_activity WHERE activity_id = %1", array(
+        1 => array(
+          $activity_id,
+          'Integer'
+        )
+      ));
+      self::$parentCaseId = self::fetchParentCaseId(self::$caseId);
     }
+  }
+
+  public static function fetchParentCaseId($childCaseId) {
+    if (!$childCaseId) {
+      return FALSE;
+    }
+    // retrieve custom table and - columns
+    $customGroup = civicrm_api3('CustomGroup', 'getsingle', array('name' => 'travel_parent'));
+    $customFields = civicrm_api3('CustomField', 'get', array('custom_group_id' => $customGroup['id']));
+    $cols = array();
+    foreach ($customFields['values'] as $fld) {
+      $cols[] = $fld['column_name'] . ' AS ' . $fld['name'];
+    }
+    // build and execute query to retrieve parentCaseId from custom table
+    $sql = 'SELECT ' . implode(', ', $cols) . ' FROM ' . $customGroup['table_name'] . ' WHERE entity_id=' . $childCaseId;
+    $dao = CRM_Core_DAO::executeQuery($sql);
+    if ($dao->fetch()) {
+      // data retrieved: return parentCaseId
+      return $dao->case_id;
+    }
+    else {
+      return FALSE;
+    }
+  }
 }
