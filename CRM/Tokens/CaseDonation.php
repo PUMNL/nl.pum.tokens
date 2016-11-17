b441f6ce4badacb2897af5ebadc7f05bf2c5ee00
ruudgithub <ruudgithub@users.noreply.github.com>
2016-11-17 21:51:55 +0100

    Fixed #3556: Added token for sponsor code

--- CRM/Tokens/CaseDonation.php
+++ CRM/Tokens/CaseDonation.php
@@ -22,6 +22,7 @@ class CRM_Tokens_CaseDonation {
   public function tokens(&$tokens) {
     $t = array();
     $t[$this->token_name.'.donation_for_fa'] = ts($this->token_label);
+    $t[$this->token_name.'.donation_sponsor_code'] = ts($this->token_label.' sponsor code');
     $tokens[$this->token_name] = $t;
   }
   
@@ -29,27 +30,45 @@ class CRM_Tokens_CaseDonation {
     if ($this->checkToken($tokens, 'donation_for_fa')) {
       $this->donationToken($values, $cids, 'donation_for_fa');
     }
+    if ($this->checkToken($tokens, 'donation_sponsor_code')) {
+      $this->donationSponsorCodeToken($values, $cids, 'donation_sponsor_code');
+    }
   }
   
   private function donationToken(&$values, $cids, $token) {
     if (!empty($this->case_id)) {
-      $dao = CRM_Core_DAO::executeQuery("SELECT dnr.display_name FROM civicrm_case c
-                                          LEFT JOIN civicrm_donor_link dlk ON dlk.entity_id = c.id AND dlk.entity = 'Case' AND dlk.is_fa_donor = 1
-                                          LEFT JOIN civicrm_contribution ctr ON ctr.id = dlk.donation_entity_id
-                                          LEFT JOIN civicrm_contact dnr ON dnr.id = ctr.contact_id
-                                          WHERE c.id = %1 LIMIT 1", 
-                                          array(1=>array($this->case_id, 'Integer')));
+      $donor = CRM_Core_DAO::singleValueQuery("SELECT dnr.display_name FROM civicrm_case c
+                                                LEFT JOIN civicrm_donor_link dlk ON dlk.entity_id = c.id AND dlk.entity = 'Case' AND dlk.is_fa_donor = 1
+                                                LEFT JOIN civicrm_contribution ctr ON ctr.id = dlk.donation_entity_id
+                                                LEFT JOIN civicrm_contact dnr ON dnr.id = ctr.contact_id
+                                                WHERE c.id = %1 LIMIT 1", 
+                                                array(1=>array($this->case_id, 'Integer')));
       
-      while($dao->fetch()) {
-        $value = $dao->display_name;
-        
-        foreach($cids as $cid) {
-          $values[$cid][$this->token_name.'.'.$token] = $value;
-        }
+      foreach($cids as $cid) {
+        $values[$cid][$this->token_name.'.'.$token] = $donor;
       }
     }
   }
   
+  private function donationSponsorCodeToken(&$values, $cids, $token) {
+    if (!empty($this->case_id)) {
+      $donor_id = CRM_Core_DAO::singleValueQuery("SELECT dnr.id FROM civicrm_case c
+                                                    LEFT JOIN civicrm_donor_link dlk ON dlk.entity_id = c.id AND dlk.entity = 'Case' AND dlk.is_fa_donor = 1
+                                                    LEFT JOIN civicrm_contribution ctr ON ctr.id = dlk.donation_entity_id
+                                                    LEFT JOIN civicrm_contact dnr ON dnr.id = ctr.contact_id
+                                                    WHERE c.id = %1 LIMIT 1",
+                                                    array(1=>array($this->case_id, 'Integer')));
+
+      $dao_donor_table = CRM_Core_DAO::singleValueQuery("SELECT cg.table_name FROM civicrm_custom_group cg WHERE cg.title = 'Donor details FA' LIMIT 1");
+      $dao_donor_column = CRM_Core_DAO::singleValueQuery("SELECT cf.column_name FROM civicrm_custom_field cf WHERE cf.label = 'Donor code' LIMIT 1");
+      $donor_code = CRM_Core_DAO::singleValueQuery("SELECT {$dao_donor_column} FROM {$dao_donor_table} WHERE entity_id = %1 LIMIT 1", array(1=>array($donor_id, 'Integer')));
+
+      foreach($cids as $cid) {
+        $values[$cid][$this->token_name.'.'.$token] = $donor_code;
+      }
+    }
+  }
+
   /**
    * Returns true when token in set in the tokens array
    *
